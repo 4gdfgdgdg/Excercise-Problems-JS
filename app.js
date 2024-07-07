c6b023d0b3deb8023a412334afb739b668e107a4
Mukhtar Hussain <147908690+4gdfgdgdg@users.noreply.github.com>
2024-07-07 01:06:15 -0700

    Excercie solution code

--- app.js
+++ app.js
@@ -1,17 +1,131 @@
 
-totalMarks = 550;
-var engMarks = +prompt("Type your english number");
-var mathMarks = +prompt("Type your Math number");
-var phyMarks = +prompt("Type your Physics number");
-var islMarks = +prompt("Type your Islamiat number");
-var obtainMarks = engMarks + mathMarks + phyMarks + islMarks;
-var percentage = obtainMarks/totalMarks*100;
-if (percentage >=80){
-    console.log("A++")
-}else if(percentage >= 70){
-    console.log("A Grade")
-}else if (percentage >= 60){
-    console.log("B Grade")
-} else {
-    console.log('fail')
-}
+
+Chapter#1 Probolems
+alert("Error! Please enter you valid password")
+JavaScript code
+var message = "JavaScript Alert:\n\n";
+message += "Please enter your valid password.";
+
+alert(message);
+
+var message = "JavaScript Alert:\n\n";
+message += "Welcome to Js Land:\n\n Happy Coding"
+alert(message);
+
+var message = "Javascript alert:\n\n";
+message += "Welcome to JS Land";
+
+alert(message);
+
+var message = "javascript alert:\n\n"
+message += "Happy Coding"
+alert(message)
+
+var message = "javascript alert:\n\n"
+message +=  "Hello...i can run JS through my web browser console"
+alert(message)
+
+
+Chapter# 2 Started
+
+
+
+var username = "Enter your Full Name";
+
+var message = "Hello World"
+alert(message)
+
+var stdBio = "Jhone Doe"
+alert(stdBio)
+
+var stdAge = "15 years old"
+alert(stdAge)
+
+var certificate = "Certified Mobile App Delopment"
+alert(certificate)
+
+var food = "PIZZA:\n PIZZ:\n PIZ:\n PI:\n P"
+alert(food)
+
+var email = "mhgul786@gmail.com"
+alert(email)
+
+var book = "A smarter way to learn JavaScript"
+alert(book)
+
+Chapter #3
+
+var age = "I am 29 years old"
+alert(age)
+
+var visitors = "You have visited this site 44 times"
+alert(visitors)
+
+var birthYear ="1996"
+console.log(birthYear)
+
+chapt 4
+
+var name = "John", age = 30, city = "New York";
+
+firstName
+_counter
+user123
+$price
+CamelCaseVariable
+
+1stName 
+my name 
+my-name 
+
+
+chap5
+
+var num1 = 10;
+var num2 = 5;
+var sum = num1 + num2;
+document.write("<h3>Addition:</h3>");
+document.write(num1 + " + " + num2 + " = " + sum + "<br>");
+
+Subtraction
+var num1 = 10;
+var num2 = 5;
+var difference = num1 - num2;
+document.write("<h3> Subtraction:</h3>");
+document.write(num1 + " - " + num2 + " = " + difference + "<br>");
+
+// Multiplication
+var num1 = 10;
+var num2 = 5;
+var product = num1 * num2;
+document.write("<h3>Multiplication:</h3>");
+document.write(num1 + " * " + num2 + " = " + product + "<br>");
+
+
+var num1 = 10;
+var num2 = 5;
+var product = num1 / num2;
+document.write("<h3>Division:</h3>");
+document.write(num1 + " / " + num2 + " = " + quotient + "<br>");
+
+
+Task 3: Mathematical Expressions
+
+// a. Declare a variable
+var number;
+
+// b. Show the value of variable after declaration
+document.write("<h3>a. Value after variable declaration is: " + number + "</h3>");
+
+number = 5;
+
+// d. Show the initial value of variable
+document.write("<h3>b. Initial value: " + number + "</h3>");
+
+number++;
+
+// f. Show the value after increment
+document.write("<h3>c. Value after increment is: " + number + "</h3>");
+
+
+
